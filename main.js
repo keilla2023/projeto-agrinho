9d7b091225f6d2b29c5ab7540507f80cfa52aaaa
keilla2023 <125401349+keilla2023@users.noreply.github.com>
2024-06-27 12:38:23 -0300

    edição

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-//iterando (percorrer elementos de uma lista) sobre a lista de botoes 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
         for (let j = 0; j < botoes.length; j++) {
